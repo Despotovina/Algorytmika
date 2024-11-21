efb4fefd76eac50d062cfaf753c3335bb17b39a6
MOCARZ <100408716+Despotovina@users.noreply.github.com>
2024-11-21 13:42:47 +0100

    Update binarne.cpp

--- binarne.cpp
+++ binarne.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <list>
 using namespace std;
 
 int main(){
 int s = 0;
 cin >> s;
 int l = 0;
-int p = 7;
+int p = 20;
+string lt = {1, 2, 5, 8, 9, 11, 15, 20 };
 while (l <= p) {
 	int sr = (l+p)/2;
 	if (s>p){
@@ -14,17 +16,18 @@ while (l <= p) {
 	}
 	else if(sr=s) {
 		cout << "miejsce w tabeli to " << sr <<endl;
-		return 0;
+		return 0;	
 		}
 
 	else if(s>sr) {
 	l=sr+1;
+	}
+	else if(s<sr) {
+	p=sr-1;
 
 	}
 
 }
-	
-cout << s;
 
 return 0 ;
 }
