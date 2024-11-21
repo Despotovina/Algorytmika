dedc2e46ebe9153218d284981be2133f11d80a57
MOCARZ <100408716+Despotovina@users.noreply.github.com>
2024-11-21 14:05:21 +0100

    Add files via upload

---
+++ zadanie.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int pt;
+	int zm;
+	cout<<"Wprowadz liczbe: ";
+	cin>>zm;
+	cout<<"Wprowadz potege: ";
+	cin>>pt;
+	int wynik = zm;
+	
+	if (pt == 0){
+	cout<<"wynik potegi 0 to zawsze jest 1" ;
+	return 0;
+	}
+	else if (pt % 2==1) {
+		pt=pt-1;
+    	while(pt>0){
+    	wynik = wynik * wynik;
+    	pt=pt/2;
+	    cout<<"krok";
+	    }
+	cout<<"wynik to: "<<wynik;
+	}
+	else {
+    	while(pt>0){
+    	wynik = wynik * wynik;
+    	pt=pt/2;
+	    cout<<"krok";
+	    cout<<pt;
+	    }
+	cout<<"wynik: "<<wynik;
+	}
+    return 0;
+}
