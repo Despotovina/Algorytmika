d75a454d54c49d3460bdb59a18a8e8659ff06795
MOCARZ <100408716+Despotovina@users.noreply.github.com>
2025-01-22 20:57:25 +0100

    Add files via upload

---
+++ libinar.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int binar(int liczba)
+{
+	int i=0,tab[31];
+
+	while(liczba>0)
+	{
+		tab[i++]=liczba%2;
+		liczba/=2;
+	}
+
+	for(int j=i-1;j>=0;j--)
+		cout<<tab[j];
+}
+
+int main()
+{
+	int liczba;
+
+	cout<<"Podaj liczbę: ";
+	cin>>liczba;
+
+	cout<<" Postać binarna: ";
+	binar(liczba);
+
+	return 0;
+}
