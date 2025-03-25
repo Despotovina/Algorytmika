b718dd00ff52dd533ca034d92aec21d82a150a5e
MOCARZ <100408716+Despotovina@users.noreply.github.com>
2025-03-25 23:12:27 +0100

    Add files via upload

---
+++ nezeli.cpp
@@ -0,0 +1,36 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int wiel;
+    int x = 4;
+    int t;
+    int d;
+    int j;
+    int z;
+    cout << "Podaj stopien wielomianu: ";
+    cin >> wiel;
+    while (x >= 0) {
+    x = x - 1;
+        cout << "Podaj współczynnik stojący przy potędze" << x << ": ";
+        cout << x;
+     if (x = 3) {
+       cin >> t;
+     }
+     else {
+         if (x = 2) {
+             cin >> d;
+         }
+         else {
+             if (x = 1) {
+                 cin >> j;
+             }
+             else {
+                     cin >> z;
+             }
+         }
+     }
+    }
+    return 0;
+}
